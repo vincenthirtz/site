92256d706e7d8422b4cc6f3ab330d9e69c1838a9
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2019-12-04 16:02:39 +0100

    sidebar init

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -3,18 +3,88 @@ import styled from "styled-components";
 import {
     Link,
 } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import CreateIcon from '@material-ui/icons/Create';
+import HomeIcon from '@material-ui/icons/Home';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+import logo from "img/logo.png";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+}));
 
 const Sidebar = props => {
     const { small } = props;
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
 
 
     return (
         <SidebarStyled small={small}>
-            <ul>
-            <li><Link to="/">Home</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-            </ul>
-
+            <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                subheader={
+                    <ListSubheader component="div" id="nested-list-subheader">
+                       <img src={logo} alt="logo" />
+        </ListSubheader>
+                }
+                className={classes.root}
+            >
+                <Link to="/">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Home" />
+                    </ListItem>
+                </Link>
+                <Link to="/blog">
+                    <ListItem button>
+                        <ListItemIcon>
+                            <CreateIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Blog" />
+                    </ListItem>
+                </Link>
+                <ListItem button onClick={handleClick}>
+                    <ListItemIcon>
+                        <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItem button className={classes.nested}>
+                            <ListItemIcon>
+                                <StarBorder />
+                            </ListItemIcon>
+                            <ListItemText primary="Starred" />
+                        </ListItem>
+                    </List>
+                </Collapse>
+            </List>
         </SidebarStyled>
     );
 }
