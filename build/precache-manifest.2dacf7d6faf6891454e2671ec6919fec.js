2deba04554a7f7f71ef1cf5daa6a6c267a385a8d
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2019-12-04 15:40:53 +0100

    skelet build

---
+++ build/precache-manifest.2dacf7d6faf6891454e2671ec6919fec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fb56e486806f184875c203e4b3641d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaf891c4468864d2b69d",
+    "url": "/static/css/main.a37ea1cf.chunk.css"
+  },
+  {
+    "revision": "682d92130111b89def59",
+    "url": "/static/js/2.cf2c2c57.chunk.js"
+  },
+  {
+    "revision": "aaf891c4468864d2b69d",
+    "url": "/static/js/main.323bccfb.chunk.js"
+  },
+  {
+    "revision": "5531a63f7560c8c9cdc7",
+    "url": "/static/js/runtime-main.79f567fe.js"
+  }
+]);
