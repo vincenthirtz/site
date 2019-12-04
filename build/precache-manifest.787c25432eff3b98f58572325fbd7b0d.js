0acb1f423912c0c6ca809b24e9792b7351609e5e
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2019-12-04 11:33:06 +0100

    test with server js 3

---
+++ build/precache-manifest.787c25432eff3b98f58572325fbd7b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4441c42f301998575c7cffe669fd7efe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda5482ced3df6b4eed6",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fb3f5a9965219176fac9",
+    "url": "/static/js/2.b728adf7.chunk.js"
+  },
+  {
+    "revision": "bda5482ced3df6b4eed6",
+    "url": "/static/js/main.833e94a4.chunk.js"
+  },
+  {
+    "revision": "5531a63f7560c8c9cdc7",
+    "url": "/static/js/runtime-main.79f567fe.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
