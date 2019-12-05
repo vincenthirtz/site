91e0f1ba86207247c2a9ec2cc5075781e217184e
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2019-12-05 15:33:30 +0100

    feat trad v1

--- src/i18n.js
+++ src/i18n.js
@@ -1,36 +1,26 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-const resources = {
-    en: {
-        translation: {
-            "siteName": "{{siteName}}",
-            "welcome": "Home",
-            "blog": "Blog",
-        }
-    },
-    fr: {
-        translation: {
-            "siteName": "{{siteName}}",
-            "welcome": "Accueil",
-            "blog": "Blog",
-        }
-    }
-};
+import Backend from 'i18next-xhr-backend';
 
 i18n
     .use(LanguageDetector)
+    .use(Backend)
     .use(initReactI18next)
     .init({
-        resources,
+        debug: true,
         lng: process.env.REACT_APP_SECONDARY_LANG,
         fallbackLng: process.env.REACT_APP_PRIMARY_LANG,
-        debug: true,
+        defaultNS: 'translation',
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
-        }
+        },
+        react: {
+            useSuspense: false
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json'
+        },
     });
-
 
 export default i18n;
