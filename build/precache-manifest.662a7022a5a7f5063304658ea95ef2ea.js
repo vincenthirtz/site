b72068f57863adb79f540c5a36a91a6dddb6eabc
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2019-12-12 16:02:56 +0100

    test deploy

---
+++ build/precache-manifest.662a7022a5a7f5063304658ea95ef2ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f12a392c38e3a8fc93317f4fa192709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b9a2ff13662bea13a48",
+    "url": "/static/css/main.a37ea1cf.chunk.css"
+  },
+  {
+    "revision": "b05caee3d12c85956f87",
+    "url": "/static/js/2.1d9fa5a9.chunk.js"
+  },
+  {
+    "revision": "4b9a2ff13662bea13a48",
+    "url": "/static/js/main.6b001dfb.chunk.js"
+  },
+  {
+    "revision": "5531a63f7560c8c9cdc7",
+    "url": "/static/js/runtime-main.79f567fe.js"
+  },
+  {
+    "revision": "690fa855062a1bf71ddebdf2a43f92c1",
+    "url": "/static/media/dg.690fa855.png"
+  },
+  {
+    "revision": "78e9f99bc3c993c9c83615d0f3d028c4",
+    "url": "/static/media/fr.78e9f99b.png"
+  },
+  {
+    "revision": "2782eee9176565036c6d33b9dd4d9535",
+    "url": "/static/media/lemarchand.2782eee9.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/static/media/uk.6f382b2d.png"
+  }
+]);
