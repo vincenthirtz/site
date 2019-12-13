ce523c3bb92e5d9ded3885c87d4707e5825c918d
Vincent Hirtz <vincent.hirtz@smartpanda.com>
2019-12-13 11:57:45 +0100

    fix(port): deploy

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3050);
